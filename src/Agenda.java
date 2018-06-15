fcab6b335183e47d74f7e3dbb701ec4c83ef15a2
iosifcostin <38808412+iosifcostin@users.noreply.github.com>
2018-06-15 22:15:54 +0300

    ysf

--- src/Agenda.java
+++ src/Agenda.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class Agenda {
 
-    private ArrayList<Person> agenda = new ArrayList<Person>();
+    private List<Person> agenda = new ArrayList<Person>();
 
     private static Person person = new Person();
 
@@ -24,43 +26,24 @@ public class Agenda {
 
     public void sterge (){
         String search = SkeletonJava.readStringConsole("Introduceti numele persoanei pe care doriti sa o stergeti : ");
-        for (Person p : agenda){
-            if (p.getNume()!=null && p.getNume().contains(search)){
-                agenda.remove(agenda.indexOf(p));
+        for (Iterator<Person> iterator = agenda.iterator(); iterator.hasNext(); ){
+            person = iterator.next();
+            if (person.getNume().contains(search)){
+                iterator.remove();
+                System.out.println("Persoana " + person.getNume() + " a fost stearsa");
             }
+
         }
-    }
+        }
+
 
     public void modifica(){
         String search = SkeletonJava.readStringConsole("Introduceti numele persoanei pe care doriti sa o modificati : ");
-        for (Person p : agenda){
-            if (p.getNume()!= null && p.getNume().contains(search)){
-
-                int verifica = SkeletonJava.readIntConsole("Varsta: ");
-                if (verifica>17 && verifica<60){
-                    p = new Student(verifica, "nume", "telefon", 1);
-                    p.setNume(SkeletonJava.readStringConsole("Nume : "));
-
-                    p.setTelefon(SkeletonJava.readStringConsole("Telefon : "));
-
-                    ((Student) p).setAnDeStudiu(SkeletonJava.readIntConsole("An de studiu : "));
-
-
-                }
-                else if (verifica>59 && verifica<121){
-                    p = new Pensionar(verifica,"nume","telefon",1);
-
-                    p.setNume(SkeletonJava.readStringConsole("Nume: "));
-
-                    p.setTelefon(SkeletonJava.readStringConsole("Telefon: "));
-
-                    ((Pensionar)p).setPensie(SkeletonJava.readIntConsole("Pensie: "));
-                }
-                else if (verifica<18 ||verifica>120)
-                {
-                    System.out.println("Varsta prea mica sau exagerat de mare");
-                }
-
+        for (Iterator<Person> iterator = agenda.iterator(); iterator.hasNext();){
+            person = iterator.next();
+            if (person.getNume().contains(search)){
+                iterator.remove();
+                adaugare();
             }
         }
     }
