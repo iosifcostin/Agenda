be95c827d8d210233e13681d736013b34c8825d9
iosifcostin <38808412+iosifcostin@users.noreply.github.com>
2018-06-15 20:32:28 +0300

    ysf

--- src/Agenda.java
+++ src/Agenda.java
@@ -4,17 +4,103 @@ public class Agenda {
 
     private ArrayList<Person> agenda = new ArrayList<Person>();
 
+    private static Person person = new Person();
 
-    public void adaugaInAgenda(Person person) {
 
-        agenda.add(person);
-
-    }
     public void print() {
         System.out.println("Aveti " + agenda.size() + " persone in agenda.");
         for (int i = 0; i < agenda.size(); i++) {
             System.out.println("-----> " + (i + 1) + " <-----" + "\n" + agenda.get(i));
         }
     }
+    public void cauta (){
+        String search = SkeletonJava.readStringConsole("Introduceti numele persoanei pe care o cautati : ");
+        for ( Person p : agenda ){
+            if (p.getNume()!= null && p.getNume().contains(search)){
+                System.out.println(p);
+            }
+        }
+    }
 
+    public void sterge (){
+        String search = SkeletonJava.readStringConsole("Introduceti numele persoanei pe care doriti sa o stergeti : ");
+        for (Person p : agenda){
+            if (p.getNume()!=null && p.getNume().contains(search)){
+                agenda.remove(agenda.indexOf(p));
+            }
+        }
+    }
+
+    public void modifica(){
+        String search = SkeletonJava.readStringConsole("Introduceti numele persoanei pe care doriti sa o modificati : ");
+        for (Person p : agenda){
+            if (p.getNume()!= null && p.getNume().contains(search)){
+
+                int verifica = SkeletonJava.readIntConsole("Varsta: ");
+                if (verifica>17 && verifica<60){
+                    p = new Student(verifica, "nume", "telefon", 1);
+                    p.setNume(SkeletonJava.readStringConsole("Nume : "));
+
+                    p.setTelefon(SkeletonJava.readStringConsole("Telefon : "));
+
+                    ((Student) p).setAnDeStudiu(SkeletonJava.readIntConsole("An de studiu : "));
+
+
+                }
+                else if (verifica>59 && verifica<121){
+                    p = new Pensionar(verifica,"nume","telefon",1);
+
+                    p.setNume(SkeletonJava.readStringConsole("Nume: "));
+
+                    p.setTelefon(SkeletonJava.readStringConsole("Telefon: "));
+
+                    ((Pensionar)p).setPensie(SkeletonJava.readIntConsole("Pensie: "));
+                }
+                else if (verifica<18 ||verifica>120)
+                {
+                    System.out.println("Varsta prea mica sau exagerat de mare");
+                }
+
+            }
+        }
+    }
+
+    public void adaugare()
+    {
+
+        int verifica = SkeletonJava.readIntConsole("Varsta: ");
+
+        if (verifica>17 && verifica<60)
+        {
+            person = new Student(verifica, "nume", "telefon", 1);
+
+            person.setNume(SkeletonJava.readStringConsole("Nume: "));
+
+            person.setTelefon(SkeletonJava.readStringConsole("Telefon: "));
+
+            ((Student) person).setAnDeStudiu(SkeletonJava.readIntConsole("An de studiu: "));
+
+            agenda.add(person);
+
+        }
+        else if (verifica>59 && verifica<121)
+        {
+            person = new Pensionar(verifica,"nume","telefon",1);
+
+            person.setNume(SkeletonJava.readStringConsole("Nume: "));
+
+            person.setTelefon(SkeletonJava.readStringConsole("Telefon: "));
+
+            ((Pensionar)person).setPensie(SkeletonJava.readIntConsole("Pensie: "));
+
+            agenda.add(person);
+
+
+        }
+        else if (verifica<18 ||verifica>120)
+        {
+            System.out.println("Varsta prea mica sau exagerat de mare");
+        }
+
+    }
 }
--- src/Meniu.java
+++ src/Meniu.java
@@ -4,94 +4,63 @@ import java.util.List;
 public class Meniu {
 
    private static Agenda agenda = new Agenda();
-   private static Person person = new Person();
+
    static final int EXIT = 6;
 
 
     public static void main(String[] args)
     {
-        adaugare();
-        afisare();
+        meniu();
+        int optiune = SkeletonJava.readIntConsole("Alege o optiune :  ");
 
-//        meniu();
-//        int optiune = SkeletonJava.readIntConsole("Alege o optiune ");
-//
-//        while(optiune!=EXIT)
-//        {
-//            if(optiune>EXIT)
-//                System.out.println("Optiune inexistenta");
-//            System.out.println("Ati ales:"+optiune);
-//
-//            switch (optiune){
-//                case 1: afisare(); break;
-//                case 3: adaugare(); break;
-//            }
-//
-//
-//            }
-//
-//
-//            optiune = SkeletonJava.readIntConsole("optiunea dvs:");
+        while(optiune!=EXIT)
+        {
+            if(optiune>EXIT)
+                System.out.println("Optiune inexistenta");
+            System.out.println("Ati ales:"+optiune);
+
+            switch (optiune){
+                case 1: afisare(); break;
+                case 2 : cautare(); break;
+                case 3: adaugare(); break;
+                case 4: modificare(); break;
+                case 5: stergere(); break;
+                case 6: System.exit(0);
+
+            }
+            meniu();
+            optiune = SkeletonJava.readIntConsole("optiunea dvs:");
+            }
+
 
 
 
     }
-
-
-
-
 
     static void afisare(){
         agenda.print();
-
     }
-
-    static void adaugare()
-    {
-
-        int verifica = SkeletonJava.readIntConsole("Varsta: ");
-
-            if (verifica>17 && verifica<60)
-            {
-              person = new Student(verifica, "nume", "telefon", 1);
-
-              person.setNume(SkeletonJava.readStringConsole("Nume: "));
-
-              person.setTelefon(SkeletonJava.readStringConsole("Telefon: "));
-
-              ((Student) person).setAnDeStudiu(SkeletonJava.readIntConsole("An de studiu: "));
-
-               agenda.adaugaInAgenda(person);
-
-            }
-            else if (verifica>59 && verifica<121)
-            {
-                person = new Pensionar(verifica,"nume","telefon",1);
-
-                person.setNume(SkeletonJava.readStringConsole("Nume: "));
-
-                person.setTelefon(SkeletonJava.readStringConsole("Telefon: "));
-
-                ((Pensionar)person).setPensie(SkeletonJava.readIntConsole("Pensie: "));
-
-                agenda.adaugaInAgenda(person);
-
-            }
-            else if (verifica<18 ||verifica>120)
-            {
-            System.out.println("Varsta prea mica sau exagerat de mare");
-            }
-
+    static void cautare (){
+        agenda.cauta();
+    }
+    static void adaugare (){
+        agenda.adaugare();
+    }
+    static void modificare (){
+        agenda.modifica();
+    }
+    static void stergere (){
+        agenda.sterge();
     }
 
 
 
     static void meniu() {
-        System.out.println("1. Afiseaza");
-        System.out.println("2. Cauta");
-        System.out.println("3. Adauga");
-        System.out.println("4. Modifica");
-        System.out.println("5. Sterge");
+        System.out.println("1. Afisare");
+        System.out.println("2. Cautare");
+        System.out.println("3. Adaugare");
+        System.out.println("4. Modificare");
+        System.out.println("5. Stergere");
         System.out.println("6. Exit");
 
     }
